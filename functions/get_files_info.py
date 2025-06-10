27556f8519807b3860c5f3dcf70cf8468673c7ff
arthwr <zenarthwr@gmail.com>
2025-06-10 13:34:10 +0500

    feat(init commit): add main.py as main entrance for ai cli agent script and get_files_info as script to allow agent to work with files

---
+++ functions/get_files_info.py
@@ -0,0 +1,23 @@
+import os
+
+
+def get_files_info(working_directory, directory=None):
+    working_directory = os.path.abspath(working_directory)
+    directory_path = os.path.abspath(os.path.join(working_directory, directory or ""))
+
+    if not directory_path.startswith(working_directory):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if not os.path.isdir(directory_path):
+        return f"Error: {directory or "."} is not a directory"
+
+    try:
+        files = []
+        for item in os.listdir(directory_path):
+            item_path = os.path.join(directory_path, item)
+            file_info = f"- {item}: file_size={os.path.getsize(item_path)}, is_dir={os.path.isdir(item_path)}"
+            files.append(file_info)
+
+        return "\n".join(files)
+    except Exception as e:
+        return f"Error listing files: {e}"
